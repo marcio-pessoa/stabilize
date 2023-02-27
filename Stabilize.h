8bb636979463f7740429eeb08bedc4df9e9711e9
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-02-27 11:12:31 -0300

    Add begin into constructor

--- Stabilize.h
+++ Stabilize.h
@@ -3,7 +3,10 @@
  * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  *
- * Change log
+ * Change log:
+ * 2023-02-26
+ *   Add begin into constructor.
+ *
  * 2023-01-15
  *   Experimental version.
  */
@@ -13,17 +16,17 @@
 
 #include "Arduino.h"
 
-class Stabilize
-{
-  public:
-    Stabilize();
-    void begin(int samples, float threshold);
-    float sample(float value);
-  private:
-    float _result;
-    int _samples;
-    int _samples_counter;
-    float _threshold;
+class Stabilize {
+ public:
+  Stabilize(int samples = 10, float threshold = 0.3);
+  void begin(int samples, float threshold);
+  float sample(float value);
+
+ private:
+  float _result;
+  int _samples;
+  int _samples_counter;
+  float _threshold;
 };
 
 #endif
